734f55797baf50ad81232ead1407049469fc2f0c
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2026-09-11 21:12:06 +0530

    chore: serving html

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -51,9 +51,56 @@ public class HttpServer implements Closeable {
     try(
       HttpServer server = new HttpServer(42069, (request) -> {
         Logger.info().println(request);
-        return new Response(Status.OK)
+
+        String target = request.getRequestLine().requestTarget();
+
+        if (target.equals("/")){
+          return new Response(Status.OK)
+            .header("Content-Type", "text/html")
+            .header("charset", "utf-8")
+            .text("""
+              <html>
+                <head>
+                  <title>HTTP/1.1</title>
+                  <meta charset="UTF-8" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+                  <link rel="preconnect" href="https://fonts.googleapis.com">
+                  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+                </head>
+                <style>
+                  .font-outfit {
+                    font-family: "Outfit", sans-serif;
+                    font-optical-sizing: auto;
+                    font-style: normal;
+                  }
+                  .font-sans {
+                    font-family: "Josefin Sans", sans-serif;
+                    font-optical-sizing: auto;
+                    font-style: normal;
+                  }
+                </style>
+                <body class="w-full h-full min-h-screen flex flex-col justify-center items-center font-sans bg-neutral-900 text-neutral-200">
+                  <h1 class="text-3xl font-bold">HTTP/1.1 Implementation in Java using TCP Sockets</h1>
+                </body>
+              </html>
+              """);
+        }
+
+        String text;
+        Status status = Status.OK;
+
+        if (target.endsWith("ping")) text = "pong";
+        else if (target.endsWith("health")) text = "healthy";
+        else {
+          status = Status.NOT_FOUND;
+          text = status.reason();
+        }
+
+        return new Response(status)
           .header("Content-Type", "text/plain")
-          .text("Hello World");
+          .text(text);
       });
     ){
 
